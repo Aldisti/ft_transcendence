b98feda1ad7f2daad79b816a0e3d1e34da263e0b
Ukudot <giovannipanico71@yahoo.it>
2023-12-20 09:36:52 +0100

    views added

--- data/django/transcendence/accounts/views.py
+++ data/django/transcendence/accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.generics import RetrieveDestroyAPIView, ListAPIView
+from accounts.serializers import CompleteUserSerializer
+from accounts.models import User
+
 
 # Create your views here.
+
+@api_view(['POST'])
+def registration(request):
+    user_serializer = CompleteUserSerializer(data=request.data)
+    if not user_serializer.is_valid():
+        return Response(status=400)
+    user = user_serializer.create(user_serializer.validated_data)
+    serializer_response = CompleteUserSerializer(user)
+    return Response(serializer_response.data, status=201)
+
+class RetrieveDestroyUser(RetrieveDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = CompleteUserSerializer
+
+class ListUser(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = CompleteUserSerializer
--- data/django/transcendence/transcendence/settings.py
+++ data/django/transcendence/transcendence/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&1ve_f1=v5e9=n$(u=@dfjxh)a93!&#39qi9f2atxuqn%mafyj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
