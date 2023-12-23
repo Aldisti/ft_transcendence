209b1a2db1fe0666b333dcabae9678afde5cf2b3
aldisti <alessandrodiste99@gmail.com>
2023-12-23 19:16:10 +0100

    all files removed

--- transcendence/accounts/utils.py
+++
@@ -1,9 +0,0 @@
-class Roles:
-    ADMIN = "A"
-    MOD = "M"
-    USER = "U"
-    ROLES_CHOICES = [
-        (ADMIN, "admin"),
-        (MOD, "mod"),
-        (USER, "user"),
-    ]
--- transcendence/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from authentication import views
-
-urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('refresh/', views.refresh_token, name='refresh'),
-]
