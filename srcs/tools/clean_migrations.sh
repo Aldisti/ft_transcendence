574be1db53d57c508fde957cab67f9d4ac0402b0
aldisti <alessandrodiste99@gmail.com>
2024-01-10 15:53:55 +0100

    clean_migrations updated

--- srcs/tools/clean_migrations.sh
+++ srcs/tools/clean_migrations.sh
@@ -2,10 +2,10 @@
 
 PROJECT="./data/django/transcendence/"
 APPS=('accounts' 'authentication' 'email_manager' 'oauth2' 'two_factor_auth')
-DEL_PATH="migrations/0*"
+DEL_PATH="migrations/0"
 
 for i in ${APPS[@]}; do
-	echo "$PROJECT$i/$DEL_PATH"
-	rm -f "$PROJECT$i/$DEL_PATH"
+	echo "$PROJECT$i/$DEL_PATH"*
+	rm -f "$PROJECT$i/$DEL_PATH"*
 done
 
