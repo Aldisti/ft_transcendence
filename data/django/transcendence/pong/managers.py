8973a5ca58e99eb6e278d482c6509c3df57e4245
aldisti <alessandrodiste99@gmail.com>
2024-01-17 17:01:19 +0100

    Lobby, Match and Statistics models created

---
+++ data/django/transcendence/pong/managers.py
@@ -0,0 +1,7 @@
+
+from django.db import models
+
+from pong.models import Lobby, Match, Statistics
+
+
+
---
+++ data/django/transcendence/pong/models.py
@@ -0,0 +1,140 @@
+from django.core import validators
+from django.db import models
+
+from accounts.models import UserGame
+
+
+class Lobby(models.Model):
+
+    class Meta:
+        db_table = 'lobby'
+
+    guest = models.ForeignKey(
+        to=UserGame,
+        on_delete=models.CASCADE,
+        related_name="lobbies",
+        db_column="guest",
+    )
+    name = models.CharField(
+        db_column="name",
+        max_length=32,
+        blank=True,
+        default="",
+    )
+    host = models.CharField(
+        db_column="host",
+        max_length=32,
+        blank=True,
+        default="",
+        validators=[validators.RegexValidator(regex="^[A-Za-z0-9!?*$~_-]{5,32}$")],
+    )
+    is_tournament = models.BooleanField(
+        db_column="is_tournament",
+        default=False,
+    )
+    creation_time = models.DateTimeField(
+        db_column="creation_time",
+        auto_now_add=True,
+    )
+
+    def __str__(self) -> str:
+        return (f"id: {self.id}, "
+                f"name: {self.name}, "
+                f"host: {self.host}, "
+                f"guest: {self.guest}, "
+                f"tournament: {self.is_tournament}, "
+                f"creation_time: {self.creation_time}")
+
+
+class Match(models.Model):
+
+    class Meta:
+        db_table = 'match'
+
+    lobby_id = models.ForeignKey(
+        to=Lobby,
+        on_delete=models.CASCADE,
+        related_name="matches",
+        db_column="lobby_id",
+    )
+    player1 = models.CharField(
+        db_column="player1",
+        max_length=32,
+        validators=[validators.RegexValidator(regex="^[A-Za-z0-9!?*$~_-]{5,32}$")],
+    )
+    player2 = models.CharField(
+        db_column="player2",
+        max_length=32,
+        validators=[validators.RegexValidator(regex="^[A-Za-z0-9!?*$~_-]{5,32}$")],
+    )
+    start_time = models.DateTimeField(
+        db_column="start_time",
+        auto_now_add=True,
+    )
+    duration_time = models.IntegerField(
+        db_column="duration_time",
+        default=0,
+        help_text="match duration in seconds",
+    )
+
+    def __str__(self) -> str:
+        return (f"id: {self.id}, "
+                f"lobby_id: {self.lobby_id}, "
+                f"player1: {self.player1}, "
+                f"player2: {self.player2}, "
+                f"start_time: {self.start_time}, "
+                f"duration_time: {self.duration_time}")
+
+
+class Statistics(models.Model):
+
+    class Meta:
+        db_table = 'stats'
+
+    id = models.OneToOneField(
+        to=Match,
+        on_delete=models.CASCADE,
+        related_name="stats",
+        primary_key=True,
+        db_column="id",
+    )
+    winner = models.CharField(
+        db_column="winner",
+        max_length=32,
+        default="",
+        validators=[validators.RegexValidator(regex="^[A-Za-z0-9!?*$~_-]{5,32}$")],
+    )
+    goals_p1 = models.IntegerField(
+        db_column="goals_p1",
+        default=0,
+    )
+    goals_p2 = models.IntegerField(
+        db_column="goals_p2",
+        default=0,
+    )
+    ball_touch_p1 = models.IntegerField(
+        db_column="ball_touch_p1",
+        default=0,
+    )
+    ball_touch_p2 = models.IntegerField(
+        db_column="ball_touch_p2",
+        default=0,
+    )
+    avg_position_p1 = models.IntegerField(
+        db_column="avg_position_p1",
+        default=0,
+    )
+    avg_position_p2 = models.IntegerField(
+        db_column="avg_position_p2",
+        default=0,
+    )
+    racket_zone_p1 = models.IntegerField(
+        db_column="racket_zone_p1",
+        default=0,
+    )
+    racket_zone_p2 = models.IntegerField(
+        db_column="racket_zone_p2",
+        default=0,
+    )
+
+
