ab49e0b33691dac4b2b62bd2e43595430afd6cc1
Ukudot <giovannipanico71@yahoo.it>
2024-01-24 14:44:54 +0100

    resolved some bugs with clean migrations

--- data/pong/pong/users/models.py
+++ data/pong/pong/users/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.core import validators
 
 # Create your models here.
 
-class User(models.Model):
+class PongUser(models.Model):
     class Meta:
         db_table = "pong_user"
 
 
     username = models.CharField(
         max_length=32,
-        # TODO: mettere controlli regex
+        validators=[validators.RegexValidator(regex="^[A-Za-z0-9!?*$~_-]{5,32}$")],
         db_column="username",
         primary_key=True
     )
