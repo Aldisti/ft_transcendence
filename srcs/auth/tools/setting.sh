0abf2e31bf643fd6fe1f7c4e08623b2e9f7b5f14
aldisti <alessandrodiste99@gmail.com>
2024-02-02 14:55:10 +0100

    removed testing commands

--- srcs/auth/tools/setting.sh
+++ srcs/auth/tools/setting.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-ls
-ls *
-
 if ! test -d $PROJECT_NAME
 then
 	django-admin startproject $PROJECT_NAME
 fi
+
 python $PROJECT_NAME/manage.py makemigrations
 python $PROJECT_NAME/manage.py migrate
 python $PROJECT_NAME/manage.py runserver 0.0.0.0:8000
