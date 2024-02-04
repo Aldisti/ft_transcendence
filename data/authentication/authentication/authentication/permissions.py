1bde9274e3b30929f32e555416ab437699c8d9b0
aldisti <alessandrodiste99@gmail.com>
2024-02-04 22:15:20 +0100

    added a new permission

--- data/authentication/authentication/authentication/permissions.py
+++ data/authentication/authentication/authentication/permissions.py
@@ -6,7 +6,7 @@ from users.models import Roles
 
 class IsActualUser(IsAuthenticated):
     def has_permission(self, request, view):
-        username = request.query_params.get('username', '')
+        username = request.query_params.get('username', '') or request.data.get('username', '')
         if super().has_permission(request, view):
             return username == request.user.username
         return False
@@ -16,4 +16,11 @@ class IsAdmin(IsAuthenticated):
     def has_permission(self, request, view):
         if super().has_permission(request, view):
             return request.user.role == Roles.ADMIN
+        return False
+
+
+class IsModerator(IsAuthenticated):
+    def has_permission(self, request, view):
+        if super().has_permission(request, view):
+            return request.user.role in [Roles.ADMIN, Roles.MOD]
         return False
