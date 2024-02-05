d2ebc31f370a4b1d3d9cbfaf321819ccbc743dad
aldisti <alessandrodiste99@gmail.com>
2024-02-05 17:47:29 +0100

    tfa needs to be tested

--- data/authentication/authentication/two_factor_auth/models.py
+++ data/authentication/authentication/two_factor_auth/models.py
@@ -1,3 +1,206 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+from users.models import User
+
+from pyotp import random_base32, TOTP, totp
+from secrets import token_urlsafe
+from datetime import datetime
+
+from logging import getLogger
+
+
+logger = getLogger(__name__)
+
+
+class TFATypes:
+    SOFTWARE = 'SW'
+    EMAIL = 'EM'
+    NONE = ''
+    CHOICES = {
+        SOFTWARE: 'Software',
+        EMAIL: 'Email',
+        NONE: 'Off',
+    }
+
+
+class UserTFAManager(models.Manager):
+    def create(self, user: User):
+        user_tfa = self.model(user=user)
+        user_tfa.full_clean()
+        user_tfa.save()
+        return user_tfa
+
+    def activate(self, user_tfa, otp_type: str):
+        user_tfa.otp_type = otp_type
+        user_tfa.full_clean()
+        user_tfa.save()
+        return user_tfa
+
+    def deactivate(self, user_tfa):
+        user_tfa.otp_token = ""
+        user_tfa.url_token = ""
+        user_tfa.otp_type = TFATypes.NONE
+        user_tfa.active = False
+        user_tfa.full_clean()
+        user_tfa.save()
+        return user_tfa
+
+    def generate_otp_token(self, user_tfa):
+        if user_tfa.otp_token != "":
+            raise ValueError('OTP token already generated')
+        user_tfa.otp_token = random_base32()
+        user_tfa.full_clean()
+        user_tfa.save()
+        return user_tfa
+
+    def generate_url_token(self, user_tfa):
+        user_tfa.url_token = token_urlsafe(32)
+        user_tfa.full_clean()
+        user_tfa.save()
+        return user_tfa
+
+    def delete_url_token(self, user_tfa):
+        user_tfa.url_token = ""
+        user_tfa.full_clean()
+        user_tfa.save()
+        return user_tfa
+
+    def update_active(self, user_tfa, status: bool):
+        user_tfa.status = status
+        user_tfa.full_clean()
+        user_tfa.save()
+        return user_tfa
+
+
+class OtpCodeManager(models.Manager):
+    def create(self, user_tfa):
+        otp_code = self.model(user_tfa=user_tfa)
+        otp_code.generate_code()
+        otp_code.full_clean()
+        otp_code.save()
+        return otp_code
+
+    def generate_codes(self, user_tfa) -> list:
+        self.delete_codes(user_tfa)
+        return [self.create(user_tfa=user_tfa) for i in range(10)]
+
+    def delete_codes(self, user_tfa):
+        self.filter(user_tfa=user_tfa).delete()
+
+    def validate_code(self, user_tfa, code: str) -> bool:
+        codes = user_tfa.codes.filter(code=code)
+        if len(codes) == 1:
+            codes[0].delete()
+            return True
+        elif len(codes) > 1:
+            logger.warning(f"got double code for {user_tfa.user.username}")
+        return False
+
+
+class UserTFA(models.Model):
+    """
+    Software TFA:
+        - otp_token
+        - url_token
+        - type
+        - active
+    Email TFA:
+        - otp_token
+        - url_token
+        - type
+        - active
+    """
+    user = models.OneToOneField(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='user_tfa',
+        db_column='user',
+        primary_key=True,
+    )
+    otp_token = models.CharField(
+        db_column='otp_token',
+        max_length=48,
+        blank=True,
+        default="",
+    )
+    url_token = models.CharField(
+        db_column='url_token',
+        max_length=48,
+        blank=True,
+        default="",
+    )
+    otp_type = models.CharField(
+        db_column='type',
+        choices=TFATypes.CHOICES,
+        default=TFATypes.NONE,
+    )
+    active = models.BooleanField(
+        db_column='active',
+        default=False,
+    )
+
+    objects = UserTFAManager()
+
+    def is_software(self) -> bool:
+        return self.otp_type == TFATypes.SOFTWARE
+
+    def is_email(self) -> bool:
+        return self.otp_type == TFATypes.EMAIL
+
+    def is_activating(self) -> bool:
+        return self.otp_type != TFATypes.NONE and self.otp_token != ""
+
+    def verify_otp_code(self, code: str) -> bool:
+        otp_time = datetime.now(tz=settings.TZ)
+        if self.is_email():
+            return (TOTP(self.otp_token, interval=settings.TFA['EMAIL_INTERVAL'])
+                    .verify(
+                code,
+                for_time=otp_time,
+                valid_window=settings.TFA['EMAIL_VALID_WINDOW']
+            ))
+        elif self.is_software():
+            return TOTP(self.otp_token).verify(
+                code,
+                for_time=otp_time,
+                valid_window=settings.TFA['SOFTWARE_VALID_WINDOW'],
+            )
+        return False
+
+    def to_data(self) -> dict:
+        return {'is_active': self.active, 'type': self.otp_type}
+
+    def get_uri(self) -> str:
+        if self.otp_token == '' or self.is_email():
+            return ''
+        return totp.TOTP(self.otp_token).provisioning_uri(name=self.user.email, issuer_name='Transcendence')
+
+    def __str__(self):
+        return f"user: {self.user.username} type: {self.otp_type} active: {self.active}"
+
+
+class OtpCode(models.Model):
+    user_tfa = models.ForeignKey(
+        to=UserTFA,
+        on_delete=models.CASCADE,
+        related_name="codes",
+        db_column="user_tfa",
+    )
+    # TODO: add hash to code field
+    code = models.CharField(
+        db_column='code',
+        max_length=10,
+    )
+
+    objects = OtpCodeManager()
+
+    def generate_code(self):
+        self.code = random_base32()[:10]
+
+    class Meta:
+        db_table = "otp_code"
+
+    def __str__(self) -> str:
+        return f"username: {self.user_tfa.user.username}, code: {self.code}"
--- data/authentication/authentication/two_factor_auth/views.py
+++ data/authentication/authentication/two_factor_auth/views.py
@@ -1,3 +1,139 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import APIView, api_view, permission_classes, throttle_classes
+from rest_framework.response import Response
+
+from django.core.exceptions import ValidationError
+from django.conf import settings
+
+from two_factor_auth.models import UserTFA, OtpCode
+
+from authentication.throttles import HighLoadThrottle, MediumLoadThrottle, LowLoadThrottle
+from authorization.serializers import TokenPairSerializer
+
+from datetime import datetime
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class ManageView(APIView):
+    throttle_classes = [LowLoadThrottle]
+
+    def get(self, request) -> Response:
+        user_tfa = request.user.user_tfa
+        return Response(data=user_tfa.to_data(), status=200)
+
+    def post(self, request) -> Response:
+        tfa_type = request.data.get('type', '')
+        user_tfa = request.user.user_tfa
+        try:
+            user_tfa = UserTFA.objects.activate(user_tfa, otp_type=tfa_type)
+        except ValidationError as e:
+            return Response(data={'message': e.message}, status=400)
+        if user_tfa.is_email():
+            return Response(status=200)
+        return Response({
+            'uri': user_tfa.get_uri(),
+            'token': user_tfa.otp_token
+        }, status=200)
+
+    def put(self, request) -> Response:
+        user_tfa = request.user.user_tfa
+        if not user_tfa.active:
+            return Response(data={'message': '2fa not active'}, status=400)
+        code = request.data.get('code', '')
+        if code == '':
+            return Response(data={'message': 'code is missing'}, status=400)
+        if user_tfa.verify_otp_code(code):
+            user_tfa = UserTFA.objects.deactivate(user_tfa)
+            OtpCode.objects.delete_codes(user_tfa)
+            return Response(status=200)
+        return Response(data={'message': 'invalid code'}, status=400)
+
+
+@api_view(['POST'])
+@permission_classes([])
+@throttle_classes([HighLoadThrottle])
+def validate_login(request) -> Response:
+    url_token = request.query_params.get('token', '')
+    code = request.data.get('code', '')
+    if url_token == '' or code == '':
+        return Response(data={'message': 'missing token or code'}, status=400)
+    try:
+        user_tfa = UserTFA.objects.get(url_token=url_token)
+    except UserTFA.DoesNotExist:
+        return Response(data={'message': 'user not found'}, status=404)
+    # TODO: check if user is trying to reset the password, idk why
+    # if user_tfa.user.user_tokens.is_resetting_password():
+    #     return Response(status=403)
+    if not user_tfa.verify_otp_code(code):
+        if not OtpCode.objects.validate_code(user_tfa, code):
+            user_tfa = UserTFA.objects.generate_url_token(user_tfa)
+            return Response(data={
+                'message': 'invalid code',
+                'token': user_tfa.url_token
+            }, status=400)
+        OtpCode.objects.delete_codes(user_tfa)
+        user_tfa = UserTFA.objects.deactivate(user_tfa)
+    user_tfa = UserTFA.objects.delete_url_token(user_tfa)
+    refresh_token = TokenPairSerializer.get_token(user_tfa.user)
+    exp = datetime.fromtimestamp(refresh_token['exp'], tz=settings.TZ) - datetime.now(tz=settings.TZ)
+    response = Response(data={'access_token': str(refresh_token.access_token)}, status=200)
+    response.set_cookie(
+        key='refresh_token',
+        value=str(refresh_token),
+        max_age=exp,
+        secure=False,
+        httponly=False,
+        samesite=None,
+    )
+    return response
+
+
+@api_view(['POST'])
+@permission_classes([])
+@throttle_classes([HighLoadThrottle])
+def validate_recover(request) -> Response:
+    url_token = request.query_params.get('token', '')
+    code = request.data.get('code', '')
+    if url_token == '' or code == '':
+        return Response(data={'message': 'missing token or code'}, status=400)
+    try:
+        user_tfa = UserTFA.objects.get(url_token=url_token)
+    except UserTFA.DoesNotExist:
+        return Response(data={'message': 'user not found'}, status=404)
+    # TODO: check if user is resetting the password, again, idk y
+    # if not user_tfa.user.user_tokens.is_resetting_password():
+    #     return Response(status=403)
+    if not user_tfa.verify_otp_code(code):
+        if not OtpCode.objects.validate_code(user_tfa, code):
+            user_tfa = UserTFA.objects.generate_url_token(user_tfa)
+            return Response(data={
+                'message': 'invalid code',
+                'token': user_tfa.url_token}, status=400)
+    user_tfa = UserTFA.objects.delete_url_token(user_tfa)
+    # TODO: return the token to reset the password
+    # return Response(data={'token': user_tfa.user.user_tokens.password_token}, status=200)
+    return Response(status=200)
+
+
+@api_view(['POST'])
+@throttle_classes([MediumLoadThrottle])
+def validate_activate(request) -> Response:
+    code = request.data.get('code', '')
+    if code == '':
+        return Response(data={'message': 'missing code'}, status=400)
+    user_tfa = request.user.user_tfa
+    if not user_tfa.is_activating():
+        return Response(data={
+            'message': "2fa activation process not started yet",
+        }, status=403)
+    if not user_tfa.verify_otp_code(code):
+        UserTFA.objects.delete_url_token(user_tfa)
+        return Response(data={'message': 'invalid code'}, status=400)
+    user_tfa = UserTFA.objects.delete_url_token(user_tfa)
+    otp_codes = OtpCode.objects.generate_codes(user_tfa=user_tfa)
+    codes = [otp_code.code for otp_code in otp_codes]
+    UserTFA.objects.activate(user_tfa)
+    return Response(data={'codes': codes}, status=200)
