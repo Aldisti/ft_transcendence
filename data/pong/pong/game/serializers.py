df0aeebba112d716aca00eccc56b079d62e62ee3
Ukudot <giovannipanico314@gmail.com>
2024-02-16 11:08:59 +0100

    game matches serialization fixed

--- data/pong/pong/game/serializers.py
+++ data/pong/pong/game/serializers.py
@@ -1,9 +1,13 @@
 def serialize_game_matches(participants, opponents, games):
     data = []
     for participant, opponent, game in zip(participants, opponents, games):
+        opponent_stats = getattr(opponent, "stats", None)
+        opponent_score = getattr(opponent_stats, "score", 0)
+        participant_stats = getattr(participant, "stats", None)
+        participant_score = getattr(participant_stats, "score", 0)
         match = {
             "opponent": opponent.player_id,
-            "scores": [participant.stats.score, opponent.stats.score],
+            "scores": [participant_score, opponent_score],
             "date": game.get_created(),
         }
         data.append(match)
